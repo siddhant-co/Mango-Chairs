0d76112448c1ec56b4d8a8f8be8d8a86c4a95bdb
siddhant-co <your-new-email@example.com>
2025-05-15 18:15:26 +0530

    test

--- api/fetchdata.ts
+++ api/fetchdata.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL; 
 
-async function fetchData(endpoint: string) {
+async function fetchData(endpoint: string)  {
   try {
     const response = await axios.get(`${NEXT_PUBLIC_API_BASE_URL}/${endpoint}`);
     return response.data;
